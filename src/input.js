90bb11679a7f6044bcaa4fad9bc42357d3557ad0
KickerKerem <43823415+KickerKerem@users.noreply.github.com>
2022-01-22 22:00:40 +0300

    Initial commit

---
+++ src/input.js
@@ -0,0 +1,31 @@
+export default class InputHandler {
+  constructor(paddle) {
+    document.addEventListener("keydown", (event) => {
+      /*alert(event.code);*/
+      switch (event.code) {
+        case "ArrowLeft":
+          paddle.moveLeft();
+          break;
+        case "ArrowRight":
+          paddle.moveRight();
+          break;
+        default:
+          break;
+      }
+    });
+
+    document.addEventListener("keyup", (event) => {
+      /*alert(event.code);*/
+      switch (event.code) {
+        case "ArrowLeft":
+          if (paddle.speed < 0) paddle.stop();
+          break;
+        case "ArrowRight":
+          if (paddle.speed > 0) paddle.stop();
+          break;
+        default:
+          break;
+      }
+    });
+  }
+}
